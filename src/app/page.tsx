80b26ebb245fb76511def9cf4123cd63c9adf3fc
QbAnurag <anurag.patra@qbent.in>
2025-04-28 16:23:20 +0530

    Add new components and update dependencies
    - Introduced Footer, Pricing, and WorkUs components.
    - Updated package.json and package-lock.json to include new Radix UI dependencies.
    - Enhanced Home page with new components and a "View More" button for match listings.
    - Added custom Label, RadioGroup, and Separator components for UI consistency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,47 @@
+"use client";
+import { Footer } from "@/component/Footer";
 import { Hero } from "@/component/Hero";
 import MatchCard from "@/component/MatchCard";
 import { Navbar } from "@/component/Navbar";
+import { Pricing } from "@/component/Pricing";
+import { WorkUs } from "@/component/WorkUs";
+import { Button } from "@/components/ui/button";
 import { MatchList } from "@/lib/CricketData";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const matchesToShow = MatchList.slice(0, 6); // Show only first 6 matches
+
+  const handleViewMoreClick = () => {
+    router.push("/match-list"); // Redirects to match list page
+  };
   return (
     <div className="m-1 p-1">
       <Navbar />
       <Hero />
-      <section className="">
+      <section className="p-5">
         <div className="container">
-          <div className="mx-auto flex max-w-5xl flex-col items-center gap-6 text-center">
-            <h1 className="mb-6 text-4xl font-semibold text-pretty lg:text-5xl">
-              Upcoming Match List
-            </h1>
-
-            <div className="mt-10 grid grid-cols-1 place-items-center gap-8 sm:grid-cols-2 lg:grid-cols-3">
-              {MatchList.map((item: any) => (
-                <MatchCard key={item._id} match={item} />
-              ))}
-            </div>
+          <div className="grid gap-10 md:grid-cols-2 lg:grid-cols-3">
+            {matchesToShow.map((item: any) => (
+              <MatchCard key={item._id} match={item} />
+            ))}
           </div>
+          {MatchList.length > 6 && (
+            <div className="mt-6 text-center">
+              <Button
+                onClick={handleViewMoreClick}
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600"
+              >
+                View More
+              </Button>
+            </div>
+          )}
         </div>
       </section>
+      <Pricing />
+      <WorkUs />
+      <Footer />
     </div>
   );
 }
